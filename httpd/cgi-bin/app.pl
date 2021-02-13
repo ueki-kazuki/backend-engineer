511e379c86e244468b8371982680f318d56129b2
Kazuki Ueki <ueki.kazuki@classmethod.jp>
2021-02-13 16:40:53 +0900

    Use say function

--- httpd/cgi-bin/app.pl
+++ httpd/cgi-bin/app.pl
@@ -2,9 +2,22 @@
 
 use strict;
 use warnings;
+use 5.010;
 
-print("Content-Type: text/plain\n");
-print("\n");
+my ($sec,$min,$hour,$mday,$mon,$year) = localtime;
+
+say("Status: 301 Moved Permanetly");
+say("Location: http://www.yahoo.co.jp/");
+say("Content-Type: text/html");
+say("");
+printf("%04d/%02d/%02d %02d:%02d:%02d"
+	, $year+1900
+	, $mon +1
+	, $mday, $hour, $min, $sec);
+say("<table>");
 foreach my $key (sort keys %ENV) {
-	print("$key = $ENV{$key}\n")
+	#say("$key = $ENV{$key}")
+	#say("<li>$key = $ENV{$key}</li>")
+	say("<tr><th>$key</th><td>$ENV{$key}</td></tr>");
 }
+say("</table>");
