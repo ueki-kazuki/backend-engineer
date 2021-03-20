2cb4e6beb73f7bdc43159b968972a05cb6251a86
Kazuki Ueki <ueki.kazuki@classmethod.jp>
2021-03-20 11:56:43 +0900

    Add senario and cgi for form

---
+++ httpd/cgi-bin/07/tweet.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use 5.010;
+
+my $q = $ENV{'QUERY_STRING'};
+my @fields = split("&", $q);
+my %pairs  = {};
+for my $field (@fields) {
+    my($key, $value) = split("=", $field);
+    $pairs{$key} = $value;
+}
+
+my ($sec,$min,$hour,$mday,$mon,$year) = localtime;
+
+
+say("Content-Type: text/html");
+say("");
+say("<b>", $pairs{'yourname'}, "</b>");
+say("<p>", $pairs{'message'}, "</p>");
+say("</p>");
+printf("<p>%04d/%02d/%02d %02d:%02d:%02d</p>"
+	, $year+1900
+	, $mon +1
+	, $mday, $hour, $min, $sec);
